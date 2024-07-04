3717bd25e8710434e3a6fcf7ea1ff3fea90cc2f6
Kenza Filali <filali1991.univ@gmail.com>
2024-07-04 14:19:30 +0200

    cv updated about updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import "./About.css"
-import Me from'../../assets/kenza section about.jpg';
-import Card from '../Card/Card';
+import React from "react";
+import "./About.css";
+import Me from "../../assets/kenza section about.jpg";
+import Card from "../Card/Card";
 
 import { TbFileTypeHtml } from "react-icons/tb";
 import { FaWordpress } from "react-icons/fa6";
@@ -9,31 +9,54 @@ import { FaReact } from "react-icons/fa";
 
 function About() {
   return (
-  <>
-  <section id="about">
-    <h5>Je me présente </h5>
-    <h2>A propos de moi</h2>
-   <div className='container about__container'>
-    <div className="about__me">
-     <img src={Me} alt=" kenza's presentation"/>
-    </div>
-    <div className="about__content">
-      <div className="about__cards">
-        <Card icon={<TbFileTypeHtml/>} title="Integrateur WEB" subTitle="Depuis avr 2016"/>
-        <Card icon={<FaWordpress/>} title="Webmaster /développeur" subTitle="Depuis jan 2018"/>
-        <Card icon={<FaReact />} title="Développeuse Frontend" subTitle="Depuis jan 2021"/>
-      </div>
-      <p className="about__text"> 
-      Développeuse Frontend Avec une solide expérience de 6 ans en tant que 
-      webmaster intégrateur web, j'ai acquis une expertise polyvalente en 
-      utilisant des technologies web telles que HTML, CSS, JavaScript et WordPress, tant au sein d'entreprises que dans des projets en freelance. Mon parcours a pris une nouvelle dimension lors d'une formation 
-      professionnelle, où j'ai intensivement travaillé avec React.js pendant 2
-      ans pour faire évoluer mes compétences techniques afin qu'elles soient en adéquation avec le marché du web </p>
-   <a href="#contact" className='btn btn-primary'>Parlons-en</a>
-    </div>
-     </div>
-    </section></>
-  )
+    <>
+      <section id="about">
+        <h5>Je me présente </h5>
+        <h2>A propos de moi</h2>
+        <div className="container about__container">
+          <div className="about__me">
+            <img src={Me} alt=" kenza's presentation" />
+          </div>
+          <div className="about__content">
+            <div className="about__cards">
+              <Card
+                icon={<TbFileTypeHtml />}
+                title="Integrateur WEB"
+                subTitle="Depuis avr 2016"
+              />
+              <Card
+                icon={<FaWordpress />}
+                title="Webmaster /développeur"
+                subTitle="Depuis jan 2018"
+              />
+              <Card
+                icon={<FaReact />}
+                title="Développeuse Frontend"
+                subTitle="Depuis jan 2021"
+              />
+            </div>
+            <p className="about__text">
+              Développeuse web, passionnée par les technologies front-end,
+              toujours persévérante face aux nouveaux défis. Avec une solide
+              expérience de 6 ans dans le domaine du web, j'ai acquis une
+              expertise polyvalente en utilisant des technologies web telles que
+              HTML, CSS, JavaScript et WordPress, tant au sein d'entreprises que
+              dans des projets en freelance. Mon parcours a pris une nouvelle
+              dimension lors d'une formation professionnelle, où j'ai
+              intensivement travaillé avec le stack React.js pendant 2 ans pour
+              faire évoluer mes compétences techniques . Mon intérêt pour les
+              nouvelles technologies WEB me pousse constamment à explorer et à
+              maîtriser les dernières innovations, garantissant ainsi des
+              solutions modernes et efficaces.
+            </p>
+            <a href="#contact" className="btn btn-primary">
+              Parlons-en
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default About
+export default About;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import CallToAction from '../CallToAction/CallToAction'
-import './Header.css'
-import cv from './_Cv_kenza_Filali_pts.pdf'
-import Me from '../../assets/Header picture kenza filali.png'
-import HeaderSocials from './HeaderSocials'
-function Header({FullName}) {
+import React from "react";
+import CallToAction from "../CallToAction/CallToAction";
+import "./Header.css";
+import cv from "./cv_kenza_filali_dev_web.pdf";
+import Me from "../../assets/Header picture kenza filali.png";
+import HeaderSocials from "./HeaderSocials";
+function Header({ FullName }) {
   return (
-   <>
+    <>
+      <section>
+        <header>
+          <div className="container header__container">
+            <div className="header__infos">
+              <h5>Bonjour je suis </h5>
+              <h1>{FullName}</h1>
+              <h5 className="text-light">Développeuse Frontend</h5>
+            </div>
 
-   <section> 
-    <header>
-    <div className="container header__container">
-      <div className="header__infos"><h5>Bonjour je suis </h5>
-      <h1>{FullName}</h1>
-      <h5 className='text-light'>Développeuse Frontend</h5></div>
-      
-   
-    <div className='call__to__action'>
-      <CallToAction text="Mon CV " url={cv} />
-      <CallToAction text="Contactez moi" url="#contact"/>
-    </div>
-    <HeaderSocials/>
-    <a href="#contact" className='scroll__down'>Contactez-moi</a>
-    <div className='me'>
-    <img className='img' src={Me} alt=" Kenza Filali"/> 
-    </div>
-
-    
-    </div>
-    </header>
-   </section>
- </>
-  )
+            <div className="call__to__action">
+              <CallToAction text="Mon CV " url={cv} />
+              <CallToAction text="Contactez moi" url="#contact" />
+            </div>
+            <HeaderSocials />
+            <a href="#contact" className="scroll__down">
+              Contactez-moi
+            </a>
+            <div className="me">
+              <img className="img" src={Me} alt=" Kenza Filali" />
+            </div>
+          </div>
+        </header>
+      </section>
+    </>
+  );
 }
 
-export default Header
+export default Header;
